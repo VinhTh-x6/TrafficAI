baf76956e6945a7c720f6bf784a7b2accbea761b
VinhTh-x6 <vinhdtx6@gmail.com>
2026-04-27 15:47:40 +0700

    feat: add VideoWriter

--- track_count.py
+++ track_count.py
@@ -1,9 +1,18 @@
 import cv2
 from ultralytics import YOLO
 
-def tracking_counting(video_path, model_path):
+def tracking_counting(video_path, model_path, output_path="output.mp4"):
     model = YOLO(model_path)
     cap = cv2.VideoCapture(video_path)
+    # Lấy thông số video
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    # Tạo writer
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(output_path, fourcc, fps, (w, h))
+
     track_list = dict()
     id_set = set()
     class_counts = {
@@ -57,6 +66,7 @@ def tracking_counting(video_path, model_path):
                     y_prev = track_list[id][0][1]
                     y_curr = track_list[id][1][1]
                     if id not in id_set:
+                        # đếm xe khi chúng đi qua line từ duới lên trên
                         if y_prev >= line and y_curr < line:
                             class_counts[model.names[int(cl)]] += 1
                             id_set.add(id)
@@ -64,13 +74,15 @@ def tracking_counting(video_path, model_path):
                 label = model.names[int(cl)]
                 color = colors.get(label, (255,64,64))
                 cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
+                cv2.circle(frame, (cx, cy), 2, (191,62,255), -1)
                 # Tên class và ID của đối tượng
                 text = f"{label} ID:{id}"
                 (text_w, text_h), _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
                 pad = 4
                 cv2.rectangle(frame, (x1, y1 - text_h - 2 * pad), (x1 + text_w + 2 * pad, y1), color, -1)
                 cv2.putText(frame, text, (x1 + pad, y1 - pad), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
-    
+
+        out.write(frame)
         yield frame, class_counts    
     cap.release()
 
