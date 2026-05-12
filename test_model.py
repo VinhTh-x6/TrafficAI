5f06ac1b33f1bba8835478826ffa3c26f32adc8a
VinhTh-x6 <vinhdtx6@gmail.com>
2026-05-12 17:09:27 +0700

    feat: add requirements & test_videos

--- test_model.py
+++ test_model.py
@@ -1,12 +1,11 @@
 from ultralytics import YOLO
 
-model = YOLO(r"D:\TrafficAI\models\best.pt")
+model = YOLO(r"models/best.pt")
 model.predict(
-    source=r"D:\TrafficAI\test.mp4",
+    source=r"test_videos/test.mp4",
     imgsz=960,
     conf=0.25,
     device=0,
-    save=True,
     line_width=1,
     show=True
 )
